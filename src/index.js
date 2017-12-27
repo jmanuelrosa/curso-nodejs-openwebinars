1d385db81376c9bc16289da2174ccd9ff307a530
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-27 22:34:40 +0100

    Add home file and show with our server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+import path from 'path'
+import fs from 'fs'
 import http from 'http'
+
+const file = `./src/index.html`
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' })
-  // response.setHeader('Content-Type', 'application/json')
-  // response.statusCode = 404
 
-  if (request.method === 'GET') {
-    response.write('<h1>Metodo valido</h1>')
+  fs.readFile(file, (err, content) => {
+    if (err) {
+      return console.log(err)
+    }
+
+    response.write(content)
     return response.end()
-  }
-
-  response.write('<h1>Esta intentando acceder con un metodo no valido</h1>')
-  return response.end()
+  })
 })
 
 server.listen(8000, 'localhost', err => {
