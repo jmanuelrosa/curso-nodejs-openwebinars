5d52efff473a7f08c02f620dcc9688302d8bf2eb
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-27 22:26:17 +0100

    Add different options for headers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import http from 'http'
 
 const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' })
+  // response.setHeader('Content-Type', 'application/json')
+  // response.statusCode = 404
+
   if (request.method === 'GET') {
     response.write('<h1>Metodo valido</h1>')
     return response.end()
