5fa36d5d27a4e52e119f5f17df41d3b5bcec58a3
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-27 19:33:42 +0100

    Read file and show num characters and lines using async function

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 // Muestra el número total de lineas, y el número de palabras por linea
-// Documentación: https://nodejs.org/api/readline.html
+// Documentación: https://nodejs.org/api/fs.html
 // Ejemplo: npm start sample.txt
-const readline = require('readline');
-
+const fs = require('fs')
 const file = process.argv[2]
-let lines = 0
+
+fs.readFile(file, (err, contents) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  const lines = contents.toString().split('\n')
+
+  for(let line of lines) {
+    console.log(`Número de caracteres por linea: ${line.length}`)
+  }
+
+  console.log(`\nNúmero total de lineas: ${lines.length}`)
+})
 
 console.log(`Fichero selecionado: ${file}\n`)
-
-// Emite el evento 'line' cuando lee una linea
-const rl = readline.createInterface({
-  input: fs.createReadStream(file),
-  crlfDelay: Infinity
-})
-
-rl.on('line', (line) => {
-  ++lines
-  console.log(`Número de caracteres por linea: ${line.length}`)
-})
-
-rl.on('close', () => {
-  console.log(`\nNúmero total de lineas: ${lines}`)
-})
