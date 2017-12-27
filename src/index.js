50f0186bab272b4aa5d4228ca0bb85af1c26c7bc
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-27 20:55:42 +0100

    Add nodemon and config for our project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
-process.on('unhandledRejection', (err, p) => {
-  console.log('Custom Error: An unhandledRejection occurred')
-  console.log(`Custom Error: Rejection: ${err}`)
-})
-
-process.on('uncaughtException', err => {
-  console.log('Custom Error: An uncaughtException occurred')
-  console.log(`Custom Error: Rejection: ${err}`)
-})
-
-setTimeout(() => console.log('This will still run.'), 500)
-
-// El error es JSON.pasre
-Promise(resolve => JSON.pasre({ color: 'azul' }))
-// test()
-// throw 'casa'
+console.log('Comprobando nodemon')
+// console.log('Veremos como se recarga todo')
