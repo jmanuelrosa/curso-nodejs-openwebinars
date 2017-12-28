0e62af04301a0ea55e22c750e446301bf065e2ee
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-28 21:47:38 +0100

    Add basic express server

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.end('Hola Mundo!')
 })
 
+<<<<<<< HEAD
 app.listen('9000', () => {
   console.log('Server opened listen on http://localhost:9000')
 })
+=======
+app.listen('9000')
+>>>>>>> Add basic express server
