a10a12baf9ffae47a829e1809b66b83f2c9927f8
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-28 22:00:08 +0100

    Add config for errors, logger, parser and env

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import logger from 'morgan'
 
 const app = express()
+
+app.disable('x-powered-by')
+
+app.set('env', 'development')
+
+app.use(logger('combined'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.end('Hola Mundo!')
 })
 
-<<<<<<< HEAD
+app.use((req, res, next) => {
+  const err = new Error('La ruta no existe !!!!')
+  err.status = 404
+  next(err)
+})
+
 app.listen('9000', () => {
   console.log('Server opened listen on http://localhost:9000')
 })
-=======
-app.listen('9000')
->>>>>>> Add basic express server
