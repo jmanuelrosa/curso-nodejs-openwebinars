16c7485658d90f76679910ef7a0ac8a00363358f
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-28 21:47:38 +0100

    Add basic express server

--- src/index.js
+++ src/index.js
@@ -1,49 +1,11 @@
-// Check document is saved
-// Connect: psql postgres
-// > \l
-// > \c films
-// > \dt (show tables)
-// After save: SELECT * FROM "Film";
+import express from 'express'
 
-import Sequelize from 'Sequelize'
+const app = express()
 
-const sequelize = new Sequelize('films', '', '', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
+app.get('/', (req, res) => {
+  res.end('Hola Mundo!')
 })
 
-const Film = sequelize.define(
-  'Film',
-  {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      field: 'id',
-      primaryKey: true
-    },
-    title: {
-      type: Sequelize.STRING,
-      field: 'title'
-    },
-    poster: {
-      type: Sequelize.STRING,
-      field: 'poster'
-    }
-  },
-  {
-    freezeTableName: true
-  }
-)
-
-const newFilm = Film.sync({ force: true })
-  .then(() => {
-    return Film.create({
-      title: 'Star Wars: The Last Jedi',
-      poster: 'https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711'
-    })
+app.listen('9000', () => {
+  console.log('Server opened listen on http://localhost:9000')
 })
