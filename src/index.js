6b398680b4df47ecafc2a4471af2f570d99479e6
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-29 21:31:22 +0100

    Add css, layout and more

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res, next) => {
   })
 })
 
-app.get('/', (req, res, next) => {
+app.get('/temario', (req, res, next) => {
   res.render('temario', {
     title: 'Open Webinars'
   })
@@ -34,12 +34,6 @@ app.use(
   '/static',
   express.static(path.join(__dirname, 'public'))
 )
-
-app.use((req, res, next) => {
-  const err = new Error('La ruta no existe !!!!')
-  err.status = 404
-  next(err)
-})
 
 app.listen('9000', () => {
   console.log('Server opened listen on http://localhost:9000')
