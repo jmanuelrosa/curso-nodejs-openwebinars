63c8525fd137d177c0902c17bada01c4547ce0f6
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2017-12-29 20:53:59 +0100

    Add static files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import logger from 'morgan'
 
-import router from './router'
-
 const app = express()
 
 app.disable('x-powered-by')
@@ -18,7 +16,24 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-router(app)
+// Rutas
+app.get('/', (req, res, next) => {
+  res.render('home', {
+    title: 'Open Webinars',
+    message: 'Curso NodeJS de OpenWebinars!'
+  })
+})
+
+app.get('/', (req, res, next) => {
+  res.render('temario', {
+    title: 'Open Webinars'
+  })
+})
+
+app.use(
+  '/static',
+  express.static(path.join(__dirname, 'public'))
+)
 
 app.use((req, res, next) => {
   const err = new Error('La ruta no existe !!!!')
--- src/router.js
+++
@@ -1,9 +0,0 @@
-import home from './routes/home'
-import temario from './routes/temario'
-
-export default app => {
-  app.use('/', home)
-  app.use('/temario', temario)
-
-  return app
-}
