7fa43a21b68daead705604c3997217c72d8bc6b3
José Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2018-01-01 19:48:52 +0100

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import logger from 'morgan'
 
-import router from './router'
-
 let _server
 
 const server = {
@@ -21,25 +19,6 @@ const server = {
 
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
-
-    app.set('views', path.join(__dirname, 'views'))
-    app.set('view engine', 'pug')
-
-    // Rutas
-    router(app)
-
-    app.use(
-      '/static',
-      express.static(path.join(__dirname, 'public'))
-    )
-
-    app.use((req, res, next) => {
-      res.render('404', {
-        title: 'Open Webinars',
-        message: `La ruta no existe!!!!!`
-      })
-      next()
-    })
 
     _server = app.listen('9000', () => {
       if (process.env.NODE_ENV !== 'test') {
--- src/router.js
+++
@@ -1,9 +0,0 @@
-import home from './routes/home'
-import units from './routes/units'
-import user from './routes/user'
-
-export default app => {
-  app.use('/', home)
-  app.use('/temario', units)
-  app.use('/', user)
-}
